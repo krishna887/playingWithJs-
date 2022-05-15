28f0e4ce82e3c5b938f0067d045c86b8aef1c7fd
krishna chaulagain <66787324+krishna887@users.noreply.github.com>
2022-05-15 19:07:01 +0545

    Add files via upload

--- node.js
+++ node.js
@@ -1,6 +1,28 @@
-display=(sum)=> console.log(sum);
-calculate=(a,b,display1)=>{
-   let result=a+b;
-   display1(result);
+//this is example of callback function
+   // display=(sum)=> console.log(sum);
+   // calculate=(a,b,display1)=>{
+   //    let result=a+b;
+   //    display1(result);
+   // }
+   // calculate(5,5,display);
+
+ //  this is example of setTimeout()
+
+   // setTimeout(abc,3000);
+   // function abc() {
+   //    console.log("this content will display after 3 sec");
+   // }
+   // console.log("hello k xa yr");
+//this is also example of setTimeout() function passing as argument
+   // setTimeout(function(){ myFunction("this content will be display after 2 sec" )},2000);
+   // function myFunction(abc){
+   //    console.log(abc);
+   // }
+setInterval(time,1000);
+function time(){
+   let a=new Date();
+   document.getElementById("abc").innerHTML=
+   a.getHours()+":"+
+   a.getMinutes()+":"+
+   a.getSeconds();
 }
-calculate(5,5,display);
